02677d4b859ff6a20225423496fe2ddef45ebaf6
tillmanj <jontillman23@gmail.com>
2017-01-05 13:46:06 -0500

    more framework

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-import odoo-recipe-costing
+from . import odoo-recipe-costing
---
+++ __manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Recipe Costing",
+
+    'summary': """Recipe Management & Costing Module""",
+
+    'description': """
+        Open Academy module for managing trainings:
+            - training courses
+            - training sessions
+            - attendees registration
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Test',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo.xml',
+    ],
+}
